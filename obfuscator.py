e5f00945ef340b6703c7a8b51a4a223f5d47bca2
ramzed <pavel.lazarenko@gmail.com>
2014-08-23 15:46:54 +0400

    Где код?

--- obfuscator.py
+++ obfuscator.py
@@ -1,1 +1,24 @@
 __author__ = 'lyakovleva'
+
+FILENAME = 'dutylog'
+
+def parse(filename):
+    """
+    Function for parsing rsyslog files for the next processing
+    :param filename:
+    :return: list
+
+    """
+    assert isinstance(filename, basestring)
+    f = open(filename)
+    parsed_file = []
+    for line in f.readlines():
+        parsed_line = []
+        for field in line.split('  '):
+            if field:
+                parsed_line.append(field)
+        parsed_file.append(parsed_line)
+    return parsed_file
+        
+if __name__ == "__main__":
+    print parse(FILENAME)
